d46f421e17c838d3d9f23cd546099e60aaeec99f
marcm200 <52798815+marcm200@users.noreply.github.com>
2019-10-11 19:22:42 +0200

    Add files via upload

---
+++ _basin.bat
@@ -0,0 +1,8 @@
+@echo off
+juliatsacore_d func=z3azc cmd=basin len=11 a=1.25,0 c=0,0.025 range=4
+echo.
+echo Image '_L11_z3azc_c_ia_0_0_x_0.025_0.025_A_1.25_0.bmp_basins.bmp' contains the
+echo basins of attraction.
+echo.
+pause
+
---
+++ _periodicity.bat
@@ -0,0 +1,10 @@
+@echo off
+juliatsacore_d func=z2c cmd=period len=12 c=0.12,0.6 
+echo.
+echo Image '_L12_z2c_c_ia_0.12_0.12_x_0.6_0.6.bmp_periodicity.bmp' contains the periodic cycle
+echo and its immediate basins. Cycle direction is from thick blue line as start to thin blue line
+echo as destination.
+echo.
+pause
+
+
